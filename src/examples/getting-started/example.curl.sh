73fccfeef50726dd26acf3fe9b301ade2659fb76
Abhishek koserwal <akoserwa@redhat.com>
2026-02-03 19:16:44 +0530

    deleting v1beta1 reference

--- src/examples/getting-started/example.curl.sh
+++ src/examples/getting-started/example.curl.sh
@@ -9,7 +9,7 @@ HEADERS=(
 # endregion
 
 # region report
-REPORT_MESSAGE='{"type": "document", "reporter_type": "drive", "reporter_instance_id": "drive-1","representations": {"metadata": {"local_resource_id": "doc-123","api_href": "https://drive.example.com/document/123","console_href": "https://www.console.com/drive/documents","reporter_version": "2.7.16"},"common": {"workspace_id": "workspace-1"},"reporter": {"document_id": "doc-123","document_name": "My Important Document","document_type": "document","created_at": "2025-08-31T10:30:00Z","file_size": 2048576,"owner_id": "user-1"}}}'
+REPORT_MESSAGE='{"type": "document", "reporterType": "drive", "reporterInstanceId": "drive-1","representations": {"metadata": {"localResourceId": "doc-123","apiHref": "https://drive.example.com/document/123","consoleHref": "https://www.console.com/drive/documents","reporterVersion": "2.7.16"},"common": {"workspace_id": "workspace-1"},"reporter": {"document_id": "doc-123","document_name": "My Important Document","document_type": "document","created_at": "2025-08-31T10:30:00Z","file_size": 2048576,"owner_id": "user-1"}}}'
 
 curl "${HEADERS[@]}" \
   -X POST \
