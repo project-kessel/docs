e9d9d457a604ab11de767869f912ede5ee9b5efe
Adam0Brien <aobrien@redhat.com>
2026-05-25 12:06:35 +0100

    create initial error codes page

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,6 +64,7 @@ const baseStarlightConfig = {
           label: "Reference",
           collapsed: true,
           items: [
+            'building-with-kessel/reference/error-codes',
             'building-with-kessel/reference/schema',
             'building-with-kessel/reference/glossary',
             {
